72624b0768e1e8ee4a99c59deee162634d49dff7
lyx <fjlyx97@outlook.com>
2018-06-26 15:14:27 +0800

    增加游戏初始化参数

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMouseEvent>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //初始化棋盘
+    this->chessboardInit();
+
     //添加游戏背景图片
     QPalette palette;
     QPixmap bg(":/new/bg/image/chessboard.bmp");
@@ -13,8 +17,53 @@ MainWindow::MainWindow(QWidget *parent) :
     this->setPalette(palette);
 
 }
-
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::chessboardInit()
+{
+    for (int i = 1 ; i < 16 ; i++)
+    {
+        for (int j = 1 ; j < 16 ; j++)
+        {
+            chessboard[i][j] = 0;
+        }
+    }
+    for (int i = 1 ; i < 16 ; i++)
+    {
+        if (i == 1 && j == 1)
+        {
+            row[i] = 117;
+            col[i] = 120;
+        }
+        else
+        {
+            row[i] = row[i-1] + 40;
+        }
+    }
+}
+//键盘点击事件
+void MainWindow::mousePressEvent(QMouseEvent * e)
+{
+    //ui->test->setText(tr("(%1,%2)").arg(e->x()).arg(e->y()));
+    int putX = e->x();
+    int putY = e->y();
+    if (isGameBegin)
+    {
+        if (role == 0) //黑
+        {
+
+        }
+        else	//白
+        {
+
+        }
+    }
+}
+
+void MainWindow::singerGame()
+{
+    this->isGameBegin = True;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -14,9 +14,21 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void chessboardInit();
+    void singerGame();
+
+//重写键盘点击事件
+protected:
+    void mousePressEvent(QMouseEvent* );
+
 
 private:
     Ui::MainWindow *ui;
+    //记录棋盘
+    int chessboard[16][16]; //记录棋盘，横差50，纵差40，左上（117,120），右下（792,690）
+    int row[16] , col[16]; //记录棋盘坐标
+    bool isGameBegin; //判断游戏是否开始
+    bool role;	//0黑1白
 };
 
 #endif // MAINWINDOW_H
